7d3cb5b4587338e42cc2d74ef4e2160652bec103
migmarmx <migmsmx@gmail.com>
2024-02-29 12:29:44 -0600

    Se agtrego el proyecto

---
+++ EFCoreSP/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFCoreSP.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSP
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
